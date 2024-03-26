f596170e50b725405de97e5e553c9115116f7b10
Raph6n <naim.raphael@outlook.fr>
2024-03-26 20:14:00 +0000

    str function

--- functions.c
+++ functions.c
@@ -1,1 +1,19 @@
-#include 
+#include "function.h"
+#include <stdarg.h>
+
+int print_s(va_list argu)
+
+  {
+    int i = 0; /* count char */
+    char *string = va_arg(argu, char *); /* argu is where find args and char * the type */
+    while (string[i] !='\0') /* while not the end of the string */
+    {
+      _putchar(string[i]); /* print char*/
+      i++;
+    }
+    return (i); /* return result */
+}
+
+int print_d(va_list argu)
+{
+  
