eafd8eb424aa7cca9642f631d7ebfe822ea5fba4
malak33s <melaksarhani1bga@gmail.com>
2024-03-26 14:47:53 +0100

    add printf base code with raph and creating struture and add protypes 26/03

--- function.h
+++ function.h
@@ -1,1 +1,22 @@
-l
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdarg.h>
+
+typedef struct printf
+{
+    char let;
+    int (*printf)(va_list argu);
+}
+functions;
+
+int _printf(const char *format, ...);
+int print_c(va_list argu);
+int print_s(va_list argu);
+int print_prc(va_list argu);
+int print_d(va_list argu);
+int _putchar(char c);
+#endif
