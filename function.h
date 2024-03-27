9d6858f7097d465a57d6b4c2ffaa2cba56f315f0
malak33s <melaksarhani1bga@gmail.com>
2024-03-27 11:14:34 +0100

    pushing base printf for begenning test

--- function.h
+++ function.h
@@ -17,6 +17,6 @@ int _printf(const char *format, ...);
 int print_c(va_list argu);
 int print_s(va_list argu);
 int print_prc(va_list argu);
-int print_d(va_list argu);
+int print_dec(va_list argu);
 int _putchar(char c);
 #endif
