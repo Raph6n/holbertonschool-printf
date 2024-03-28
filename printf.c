9e3553abc6b9d7a5487fcb59b049f1f9cd8fd929
malak33s <melaksarhani1bga@gmail.com>
2024-03-28 14:46:15 +0100

    ajout de %k

--- printf.c
+++ printf.c
@@ -1,53 +1,47 @@
 #include "main.h"
 
-
 int _printf(const char *format, ...)
 {
+    int i = 0, index, printall = 0;
+    va_list argu;
 
-int i = 0, index, printall = 0;
-va_list argu;
+    functions list[] = {
+        {'c', print_c},
+        {'s', print_s},
+        {'d', print_dec},
+        {'i', print_dec},
+        {'%', print_prc},
+        {'\0', NULL},
+    };
 
- functions list[] = {
-                {'c', print_c},
-                {'s', print_s},
-                {'d', print_dec},
-                {'i', print_dec},
-                {'%', print_prc},
-                {'\0', NULL},
- };
     va_start(argu, format);
     if (format == NULL || (format[i] == '%' && format[1] == '\0'))
         return (-1);
+
     while (format && format[i])
     {
-      if (format[i] == '%' && (format[i+1] != 'K') && format [i+1] != '!')
-	{
+        if (format[i] == '%' && format[i + 1] != 'K' && format[i + 1] != '!')
         {
-
-	  index = 0;
-	  i++;
-	  while (list[index].let != '\0')
-	    {
-            index = 0;
             i++;
-        
+            index = 0;
             while (list[index].let != '\0')
-
             {
-	      if (list[index].let == format[i])
+                if (list[index].let == format[i])
                 {
-		  printall += list[index].printf(argu);
-		  break;
+                    printall += list[index].printf(argu);
+                    break;
                 }
-	      index++;
+                index++;
             }
         }
-      else
-	{
-	  _putchar(format[i]);
-	  printall++;
-	}
-      i++;
-  
-  return(printall);
+        else
+        {
+            _putchar(format[i]);
+            printall++;
+        }
+        i++;
+    }
+    
+    va_end(argu);
+    return printall;
 }
