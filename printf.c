0e5301dd32089eb83ae664b53a4b38cdd3e350c7
Houcinetb <tiabh16@gmail.com>
2024-03-28 10:20:39 +0000

    bettyprintf

--- printf.c
+++ printf.c
@@ -1,46 +1,48 @@
 #include "main.h"
 /**
  * _printf - function that print
- * 
+ * @format: var
+ * Return: printall
+ *
 */
 int _printf(const char *format, ...)
 {
 
- int i = 0, index, printall = 0;
- va_list argu;
+int i = 0, index, printall = 0;
+va_list argu;
 
- functions list[] = {
-                {'c', print_c},
-                {'s', print_s},
-                {'d', print_dec},
-                {'i', print_dec},
-                {'%', print_prc},
-                {'\0', NULL},
- };
-    va_start(argu, format);
+functions list[] = {
+{'c', print_c},
+{'s', print_s},
+{'d', print_dec},
+{'i', print_dec},
+{'%', print_prc},
+{'\0', NULL},
+};
+va_start(argu, format);
 
-    while (format && format[i])
-    {
-        if (format[i] == '%' && (format[i+1] != 'K') && format [i+1] != '!')
-        {
-            index = 0;
-            i++;
-            while (list[index].let != '\0')
-            {
-                if (list[index].let == format[i])
-                {
-                    printall += list[index].printf(argu);
-                    break;
-                }
-                index++;
-            }
-        }
-        else
-            {
-                _putchar(format[i]);
-                printall++;
-            }
-        i++;
-    }
-    return(printall);
+while (format && format[i])
+{
+if (format[i] == '%' && (format[i + 1] != 'K') && format[i + 1] != '!')
+		{
+index = 0;
+i++;
+while (list[index].let != '\0')
+{
+if (list[index].let == format[i])
+{
+printall += list[index].printf(argu);
+break;
+}
+index++;
+}
+}
+else
+{
+_putchar(format[i]);
+printall++;
+}
+i++;
+}
+return (printall);
 }
